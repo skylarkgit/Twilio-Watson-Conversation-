eb006d62351faa4bef4363e3f40cff981c91d7cb
sky1ark <abhay199658@gmail.com>
2019-09-16 19:20:11 +0530

    [upd] a demo app without twilio

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ var app = express();
 
 var contexts = [];
 
+// localhost:3000/smssent?Body=Hi&From=9911991199&To=9911991199
 app.get('/smssent', function (req, res) {
   var message = req.query.Body;
   var number = req.query.From;
-  var twilioNumber = req.query.To;
 
   var context = null;
   var index = 0;
   var contextIndex = 0;
   contexts.forEach(function(value) {
-    console.log(value.from);
     if (value.from == number) {
       context = value.context;
       contextIndex = index;
@@ -22,7 +21,7 @@ app.get('/smssent', function (req, res) {
     index = index + 1;
   });
 
-  console.log('Recieved message from ' + number + ' saying \'' + message  + '\'');
+  console.log(number,': ', message);
 
   var conversation = new AssitantV1({
     disable_ssl_verification: true,
@@ -30,9 +29,6 @@ app.get('/smssent', function (req, res) {
     url: "<api_url>",
     version: '2018-02-16'
   });
-
-  console.log(JSON.stringify(context));
-  console.log(contexts.length);
 
   conversation.message({
     input: { text: message },
@@ -42,7 +38,7 @@ app.get('/smssent', function (req, res) {
        if (err) {
          console.error(err);
        } else {
-         console.log(response.output.text[0]);
+         console.log('WATSON:', response.output.text[0]);
          if (context == null) {
            contexts.push({'from': number, 'context': response.context});
          } else {
@@ -50,27 +46,13 @@ app.get('/smssent', function (req, res) {
          }
 
          var intent = response.intents[0] && response.intents[0].intent;
-         console.log(intent);
+        //  console.log(intent);
          if (intent == "done") {
            //contexts.splice(contexts.indexOf({'from': number, 'context': response.context}),1);
            contexts.splice(contextIndex,1);
            // Call REST API here (order pizza, etc.)
          }
 
-         var client = require('twilio')(
-           '',
-           ''
-         );
-
-         client.messages.create({
-           from: twilioNumber,
-           to: number,
-           body: response.output.text[0]
-         }, function(err, message) {
-           if(err) {
-             console.error(err.message);
-           }
-         });
        }
   });
 
